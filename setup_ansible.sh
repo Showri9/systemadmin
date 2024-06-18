c46a7a8d97b96a7f72934ac829dae64e4ac8649e
Venkata Sai Showri Konda <kvsshowri@Venkatas-MacBook-Air.local>
2024-06-18 19:25:55 -0400

    edited

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -4,6 +4,6 @@ cd
 git clone https://github.com/do-community/ansible-playbooks.git
 cd ansible-playbooks/lamp_ubuntu1804/
 cp /local/repository/default.yml vars/
-HOST=$(hostname -f)
+HOST=$(ssh -o StrictHostKeyChecking=no host hostname -f)
 sed -i "s/HOSTNAME/$HOST/g" vars/default.yml
 ansible-playbook playbook.yml -l server1 -u lngo
